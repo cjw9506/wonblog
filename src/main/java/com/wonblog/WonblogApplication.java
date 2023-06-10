a1eaa2863d88e54e90aab6b611caeb0dbc404052
jeongjiwon <jungkt0x@naver.com>
2023-06-10 21:46:44 +0900

    Initial commit

---
+++ src/main/java/com/wonblog/WonblogApplication.java
@@ -0,0 +1,13 @@
+package com.wonblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonblog/WonblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
