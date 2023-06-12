b84ad0e789b5de8281eacf02f9c8ca54a64625bb
jeongjiwon <jungkt0x@naver.com>
2023-06-12 23:36:28 +0900

    add post entity

---
+++ src/main/java/com/wonblog/api/domain/Post.java
@@ -0,0 +1,30 @@
+package com.wonblog.api.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+//    public String getTitle() {
+//        return this.title.substring(0, 10);
+//    }
+}
