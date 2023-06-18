f91c0697a187b6a74ec7164652e5275dd9c17a44
jeongjiwon <jungkt0x@naver.com>
2023-06-18 23:40:18 +0900

    fix package route

--- src/main/java/com/wonblog/api/WonblogApplication.java
+++ src/main/java/com/wonblog/api/WonblogApplication.java
@@ -1,4 +1,4 @@
-package com.wonblog;
+package com.wonblog.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/wonblog/WonblogApplicationTests.java
+++ src/test/java/com/wonblog/WonblogApplicationTests.java
@@ -1,9 +1,11 @@
 package com.wonblog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration //뭥미
 class WonblogApplicationTests {
 
 	@Test
