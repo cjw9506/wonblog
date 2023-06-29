b4e46104395494d81414802c12be1b1e6f434c9f
jeongjiwon <jungkt0x@naver.com>
2023-06-29 21:29:47 +0900

    fix

---
+++ src/main/java/com/wonblog/api/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.wonblog.api.repository;
+
+import com.wonblog.api.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+}
